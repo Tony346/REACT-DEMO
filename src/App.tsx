654601a12c36a3a63aa9f09c05afcc46de77e021
tianhouju <tianhouju@360.cn>
2023-06-16 18:32:43 +0800

    改变

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ReactEventHandler } from "react";
 import "./App.scss";
 
 function App() {
@@ -7,20 +7,21 @@ function App() {
     setIsShow(true);
   };
   const handleImgError = (e: any) => {
-    console.log("图片加载出错", e);
+    console.log("e", e.target);
+    e.target.src = "https://p3.ssl.qhimg.com/t011d7a9a61e6d6be72.png";
   };
-  useEffect(() => {
-    window.addEventListener(
-      "error",
-      function (event) {
-        const target = event.target;
-        if (target instanceof HTMLImageElement) {
-          console.log("图片加载异常", target);
-        }
-      },
-      true
-    );
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener(
+  //     "error",
+  //     function (event) {
+  //       const target = event.target;
+  //       if (target instanceof HTMLImageElement) {
+  //         console.log("图片加载异常", target);
+  //       }
+  //     },
+  //     true
+  //   );
+  // }, []);
   return (
     <>
       <button onClick={handleClik}>点击出现弹窗</button>
